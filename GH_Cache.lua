e53d17b6a5d218646999bcc77d7923bab3844c5f
Raphaël Daumas <raphdaumas@hotmail.fr>
2020-12-05 11:53:04 +0100

    Update itemLink error
    Print what itemLink is causing an error when error appears

--- GH_Cache.lua
+++ GH_Cache.lua
@@ -1,33 +1,36 @@
 local function GetItemFromCache(itemLink)
-	GearHelper:BenchmarkCountFuncCall("GetItemFromCache")
-	for k, v in pairs(GearHelper.db.global.ItemCache) do
-		if k == itemLink then
-			return v
-		end
-	end
-	return nil
+    GearHelper:BenchmarkCountFuncCall("GetItemFromCache")
+    for k, v in pairs(GearHelper.db.global.ItemCache) do
+        if k == itemLink then
+            return v
+        end
+    end
+    return nil
 end
 
 local function PutItemInCache(itemLink, item)
     GearHelper:BenchmarkCountFuncCall("PutItemInCache")
     item.date = date("%y-%m-%d")
-	GearHelper.db.global.ItemCache[itemLink] = item
+    GearHelper.db.global.ItemCache[itemLink] = item
 end
 
 function GearHelper:GetItemByLink(itemLink)
-	self:BenchmarkCountFuncCall("GearHelper:GetItemByLink")
+    self:BenchmarkCountFuncCall("GearHelper:GetItemByLink")
 
-	local item = GetItemFromCache(itemLink)
+    local item = GetItemFromCache(itemLink)
 
-	if not item then
-		item = self:BuildItemFromTooltip(itemLink)
-		PutItemInCache(itemLink, item)
+    if not item then
+        item = self:BuildItemFromTooltip(itemLink)
+        if (false == item) then -- there was an error
+            return
+        end
+        PutItemInCache(itemLink, item)
     else
         item.date = date("%y-%m-%d")
         GearHelper.db.global.ItemCache[itemLink] = item
     end
 
-	return item
+    return item
 end
 
 function GearHelper:CheckCacheDate()
@@ -51,6 +54,6 @@ function GearHelper:CheckCacheDate()
 end
 
 function GearHelper:ResetCache()
-	self:BenchmarkCountFuncCall("GearHelper:ResetCache")
-	self.db.global.ItemCache = {}
+    self:BenchmarkCountFuncCall("GearHelper:ResetCache")
+    self.db.global.ItemCache = {}
 end
