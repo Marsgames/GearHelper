d393c2eb8649bb395a9d50b2cb00ba24b020f276
Marsgames <raphdaumas@hotmail.fr>
2022-12-03 12:26:54 +0100

    Fix ilvl text not updating

--- GH_CharacterFrame.lua
+++ GH_CharacterFrame.lua
@@ -7,7 +7,7 @@ local function CharFrameShow(_)
 
     table.foreach(
         GearHelperVars.charInventory,
-        function(slotName, item, number)
+        function(slotID, item, number)
             if (item ~= -1) then
                 local slotConverter = {
                     -- Left
@@ -38,12 +38,12 @@ local function CharFrameShow(_)
                 local parentAnchor = ""
                 local childAnchor = ""
 
-                if (slotName < 6 or slotName == 9 or slotName == 15 or slotName == 19) then
+                if (slotID < 6 or slotID == 9 or slotID == 15 or slotID == 19) then
                     xOffset = 5
                     yOffset = 0
                     parentAnchor = "RIGHT"
                     childAnchor = "LEFT"
-                elseif (slotName < 16) then
+                elseif (slotID < 16) then
                     xOffset = -5
                     yOffset = 0
                     parentAnchor = "LEFT"
@@ -55,10 +55,10 @@ local function CharFrameShow(_)
                     childAnchor = "BOTTOM"
                 end
 
-                local name = slotConverter[slotName]
+                local name = slotConverter[slotID]
                 local pFrame = _G[name]
 
-                local button = _G["charIlvlButton" .. name] or CreateFrame("Frame", "charIlvlButton" .. name, CharacterFrame) --    Our frame
+                local button = _G["charIlvlSlot" .. slotID] or CreateFrame("Frame", "charIlvlSlot" .. slotID, CharacterFrame) --    Our frame
                 button:SetPoint(childAnchor, pFrame, parentAnchor, xOffset, yOffset)
                 button:SetSize(50, 25)
                 button:SetFrameStrata("HIGH")
@@ -71,6 +71,7 @@ local function CharFrameShow(_)
                 local itemLink, iR, itemLevel, itemEquipLoc = itemScan.itemLink, itemScan.rarity, itemScan.iLvl, itemScan.equipLoc
 
                 if (itemLevel > 0) then
+                    button:ClearLines()
                     text:SetText(itemLevel)
                     local r, g, b = GetItemQualityColor(iR and iR or 0)
                     text:SetTextColor(r, g, b, 1)
@@ -92,11 +93,18 @@ end
 function GearHelper:HideIlvlOnCharFrame()
     table.foreach(
         GearHelperVars.charInventory,
-        function(slotName, _)
-            if (_G["charIlvlButton" .. slotName]) then
-                _G["charIlvlButton" .. slotName]:Hide()
-                _G["charIlvlButton" .. slotName] = nil
+        function(slotID, _)
+            if (_G["charIlvlSlot" .. slotID]) then
+                _G["charIlvlSlot" .. slotID]:Hide()
+                _G["charIlvlSlot" .. slotID]:ClearLines()
             end
         end
     )
+end
+
+function GearHelper:ResetIlvlOnCharFrame()
+    CharFrameHide()
+    if (PaperDollItemsFrame:IsShown()) then
+        CharFrameShow()
+    end
 end
