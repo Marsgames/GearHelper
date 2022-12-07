ff042a7a1879b2b14af6cccae6173bd4ada3d879
Marsgames <raphdaumas@hotmail.fr>
2022-12-07 02:08:49 +0100

    Fix partially posedot

--- Inventory/GH_ShowUpgradeIcon.lua
+++ Inventory/GH_ShowUpgradeIcon.lua
@@ -1,17 +1,16 @@
 function GearHelper:ShowUpgradeOnItemsIcons()
     for bagId, items in pairs(GearHelperVars.bagsItems) do
+        local bagSize = C_Container.GetContainerNumSlots(bagId)
         for _, itemInfo in pairs(items) do
-
-            local button = _G["ContainerFrame"..bagId.."Item"..itemInfo.slot]
+            local button = _G["ContainerFrame" .. bagId + 1 .. "Item" .. (bagSize + 1) - itemInfo.slot]
 
             if button then
                 if button.overlay then
                     button.overlay:SetShown(false)
                     button.overlay = nil
                 end
-
                 if self:IsItemBetter(itemInfo.item) and not button.overlay then
-                    GearHelper:Print("ShowUpgradeOnItemsIcons - "..itemInfo.item.itemLink.." is better")
+                    GearHelper:Print("ShowUpgradeOnItemsIcons - " .. itemInfo.item.itemLink .. " is better")
                     button.overlay = button:CreateTexture(nil, "OVERLAY")
                     button.overlay:SetSize(18, 18)
                     button.overlay:SetPoint("TOPLEFT")
@@ -21,8 +20,6 @@ function GearHelper:ShowUpgradeOnItemsIcons()
             end
         end
     end
+
     ContainerFrame_UpdateAll()
 end
-
---EventRegistry:RegisterCallback("ContainerFrame.OpenBag", self.CheckOpenInventory, self);
---EventRegistry:UnregisterCallback("ContainerFrame.OpenBag", self);
