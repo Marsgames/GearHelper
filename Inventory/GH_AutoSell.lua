f20721e3649c3798b8fc9642443a450f92ff6e10
Marsgames <raphdaumas@hotmail.fr>
2023-09-27 23:09:25 +0200

    Fix securecall

--- Inventory/GH_AutoSell.lua
+++ Inventory/GH_AutoSell.lua
@@ -6,12 +6,12 @@ function GearHelper:SellGreyItems()
     end
 
     for bag = Enum.BagIndex.Backpack, NUM_TOTAL_EQUIPPED_BAG_SLOTS do
-        for slot = 1, C_Container.GetContainerNumSlots(bag) do
-            local infos = C_Container.GetContainerItemInfo(bag, slot)
+        for slot = 1, securecall(C_Container.GetContainerNumSlots, bag) do
+            local infos = securecall(C_Container.GetContainerItemInfo, bag, slot)
             if (infos) then
                 local quality = infos.quality
                 if (quality and quality == 0) then
-                    securecall(UseContainerItem, bag, slot)
+                    securecall(C_Container.UseContainerItem, bag, slot)
                 end
             end
         end
